dfd34126b7cb0e7f12779df7a8110f06687e9af3
Niko Warner <niko@levion.com>
2012-08-30 19:11:31 -0700

    OK, here we go again!

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 redis_uri = ( Rails.env == "production" ? ENV["REDISTOGO_URL"] : "redis://127.0.0.1:6379/" )
-ReservationBooker::Application.config.session_store :redis_store, { :servers => redis_uri + "1/res:session", :expire_in => 3600 }
+ReservationBooker::Application.config.session_store :redis_store, { :servers => redis_uri + "res:session", :expire_in => 3600 }
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
