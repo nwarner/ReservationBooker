3306643505cfab13b4a05e3a4ad86fbd558700ed
Niko Warner <niko@levion.com>
2012-08-30 19:08:47 -0700

    Fixed config for Heroku deployment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+REDIS_URI =  URI.parse( Rails.env == "production" ? ENV["REDISTOGO_URL"] : "redis://127.0.0.1:6379/" )
+REDIS = Redis.new(:host => REDIS_URI.host, :port => REDIS_URI.port, :password => REDIS_URI.password)
 Resque.redis = REDIS
